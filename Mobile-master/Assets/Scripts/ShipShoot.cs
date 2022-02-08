1c5cb6579cb85e8a4e9c273d8f4b510f6844f77d
HypeDrivenDevelopment <38166479+HypeDrivenDevelopment@users.noreply.github.com>
2022-02-09 01:55:15 +0200

    mobile game

---
+++ Mobile-master/Assets/Scripts/ShipShoot.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipShoot : MonoBehaviour
+{
+    public GameObject m_BallPrefab;
+    public Transform m_LaunchTransform;
+
+    private void Update()
+    {
+        Shoot();
+    }
+
+    public void Shoot()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GameObject ballClone = Instantiate(
+                m_BallPrefab,
+                m_LaunchTransform.position,
+                Quaternion.identity);
+        }
+
+        // Havaitaan kosketukset.
+        if (Input.touchCount > 0)
+        {
+            // Kosketuksesta muuttuja.
+            Touch touch = Input.GetTouch(0);
+            switch (touch.phase)
+            {
+                // Luodaan kosketuksen alkuvaiheessa pallo.
+                case TouchPhase.Began:
+            
+                    GameObject ballClone = Instantiate(
+                    m_BallPrefab,
+                    m_LaunchTransform.position,
+                    Quaternion.identity);
+                    break;
+            }
+
+        }
+    }
+}
+        
+
+
