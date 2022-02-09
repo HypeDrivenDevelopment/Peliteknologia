9ea93cabcbcc47f1e88e17e3c5a1747d24b9a722
HypeDrivenDevelopment <38166479+HypeDrivenDevelopment@users.noreply.github.com>
2022-02-09 02:44:55 +0200

    vuforiademo

---
+++ VuforiaDemo/Assets/SpinToWin.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinToWin : MonoBehaviour {
+
+    // Pyörimisen nopeus
+    int Speed = 100;
+	
+	void Update () {
+        // Joka updatella pallon y-akselin rotaatiota muutetaan hieman.
+        transform.Rotate(new Vector3(0, Time.deltaTime * Speed, 0));
+	}
+}
